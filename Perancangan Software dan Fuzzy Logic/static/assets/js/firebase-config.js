4291fe81925ddc0c0939ea1825f4f935700d9b0e
dastinaryo5@gmail.com <dastinaryo2@gmail.com>
2024-01-02 12:40:26 +0700

    initial commit

---
+++ Perancangan Software dan Fuzzy Logic/static/assets/js/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
